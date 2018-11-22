57bf27f20292bb645d1763c1b049349d3e03510d
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2018-11-22 15:09:56 -0600

    Make action class

---
+++ actions/actionMain.gd
@@ -0,0 +1,8 @@
+var cost_MP = 0
+var cost_HP = 0
+
+func _ready():
+	pass
+	
+func action():
+	print("Something happened")
--- items/scripts/ItemMain.gd
+++ items/scripts/ItemMain.gd
@@ -1,9 +1,12 @@
-#The function that is run when the item is used
-var useFunc = null
 #The weight of the item
 var weight = 0
 #The cost on Ladrahasla Stamina
 var cost_MP = 0
+#The action used by this item
+var action = load("res://actions/actionMain.gd").action
+
+func _ready():
+	pass
 
 func use(attacker, defender):
-	useFunc(attacker, defender)
+	action(attacker, defender)
