d73afd4c98f01741deef802732a9ef4c84e39f65
archfan <33993466+archfan7411@users.noreply.github.com>
2018-11-22 11:36:34 -0500

    Add diagonal positioning

--- characters/side/scripts/Side_character.gd
+++ characters/side/scripts/Side_character.gd
@@ -3,7 +3,8 @@ extends "res://characters/scripts/PlayerMain.gd"
 var class_data = load("res://characters/scripts/PartyList.gd")
 var party = class_data.new()
 var character_index = 0
-var spacing = 50
+var spacing_y = 100
+var spacing_x = 100
 var offset_x = 800
 var offset_y = 100
 
@@ -25,8 +26,10 @@ func _ready():
 	for i in party.party:
 		if party.party[i].name == character_name:
 			character_index = i
-	
-	position = Vector2(offset_x,character_index * spacing + offset_y)
+	if character_index % 2 == 0: # If character is an even index
+		position = Vector2(offset_x, character_index * spacing_y + offset_y)
+	else:
+		position = Vector2(offset_x + spacing_x, character_index * spacing_y + offset_y)
 
 func update_movement(delta):
 	# check which state the character is in.
